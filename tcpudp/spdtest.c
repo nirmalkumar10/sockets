7b33f34cc2ded7abf32c2b2c708b38dff3969398
nirmalkumar <nirmal4491@yahoo.com>
2016-03-06 11:25:06 -0800

    new commit

--- tcpudp/spdtest.c
+++ tcpudp/spdtest.c
@@ -1,7 +1,16 @@
+/*
+File        : spdtest.c
+Author      :Nirmal Kumar Venkatesan
+SJSU ID     :010690706
+Description :UDP client writes a message of specified size
+             to the server and waits for an ack from server.
+	     Calculates throughput based on total message sent over
+	     the specified delay.  	
+*/
 
 #include "common.h"
 
-#define MSGCOUNT 1
+#define MSGCOUNT 30
 #define START_SEQ 30
 
 int main(int argc,char *argv[])
@@ -9,19 +18,20 @@ int main(int argc,char *argv[])
 	struct sockaddr_in server,client;
 	struct timeval  tv1, tv2;
 	socklen_t slen;
-	int s, portno,msgsize,ack,msgcount,seqno;
+	int s, portno,msgsize,ack,msgcount,seqno,total_message_size;
 	char *buf;
 	double time_elapsed;
+
 	msgcount = MSGCOUNT;
 	portno = atoi(argv[2]);
 	msgsize = atoi(argv[3]);
 	slen = sizeof(server);
 	seqno = START_SEQ;	
-	
+
 	buf = (char *)malloc((sizeof(char) * msgsize));
-	buf[0] = (seqno & 0x00FF);
-	buf[1] = ((seqno >> 8 ) & 0xFF);
+
 	if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP))==-1)
+		perror("Error in creating socket\n");
 
 	memset((char *) &server, 0, sizeof(server));
 	server.sin_family = AF_INET;
@@ -32,17 +42,36 @@ int main(int argc,char *argv[])
 	}
 	gettimeofday(&tv1, NULL);
 	while(msgcount) {	
-	sendto(s, buf,msgsize, 0,(struct sockaddr *)&server,slen);
-	recvfrom(s,&ack,sizeof(ack),0,(struct sockaddr *)&client,&slen);
-	msgcount--;
+		buf[0] = (seqno & 0x00FF);                           /* LSB of sequence number in first byte of buffer */
+		buf[1] = ((seqno >> 8 ) & 0xFF);                     /*MSB of sequence number in second byte of buffer */
+		sendto(s, buf,msgsize, 0,(struct sockaddr *)&server,slen);
+		recvfrom(s,&ack,sizeof(ack),0,(struct sockaddr *)&client,&slen);
+		debug_print("Ack received:%d\n",ack);
+		seqno++;
+		msgcount--;
 	}
 	gettimeofday(&tv2, NULL);
 
 
 	time_elapsed = (double) (tv2.tv_sec - tv1.tv_sec) + ((double)(tv2.tv_usec - tv1.tv_usec) / 1000000 )  ;
-	printf("time elapsed :%f seconds",time_elapsed);
-	printf("Total message size :%d\r\n",MSGCOUNT * msgsize);
-	printf("Throughput :%f\r\n", (MSGCOUNT * msgsize) / time_elapsed);
+	debug_print("time elapsed :%f seconds",time_elapsed);
+	debug_print("Total message size :%d\r\n",MSGCOUNT * msgsize);
+
+	total_message_size = (MSGCOUNT * msgsize * 8);
+
+	if(total_message_size > 1000){
+		total_message_size = (total_message_size /1000);
+		printf("Throughput :%f Kbps \n",total_message_size / time_elapsed );
+	}
+
+	if(total_message_size > 1000000){
+		total_message_size = (total_message_size /1000000);
+		printf("Throughput :%f Mbps \n",total_message_size / time_elapsed );
+	}
+	if(total_message_size > 1000000000){
+		total_message_size = (total_message_size /1000000000);
+		printf("Throughput :%f Gbps \n",total_message_size / time_elapsed );
+	}
 	close(s);
 	return 0;
 }
--- tcpudp/spdtestd.c
+++ tcpudp/spdtestd.c
@@ -10,12 +10,12 @@ Description :UDP server reads a message of specified
 
 #include "common.h"
 
-#define MSGCOUNT 1
+#define MSGCOUNT 30
 #define START_SEQ 30
 
 int main(int argc,char *argv[])
 {
-	struct sockaddr_in server, si_other;
+	struct sockaddr_in server, client;
 	socklen_t slen;
 	int s, portno,msgsize,slptime,ack,msgcount,seqno,recvd;
 	char *buf;
@@ -28,11 +28,12 @@ int main(int argc,char *argv[])
 	portno = atoi(argv[1]);
 	msgsize = atoi(argv[2]);
 	slptime = atoi(argv[3]);
-	slen = sizeof(si_other);	
+	slen = sizeof(client);	
 
-	buf = (char *)malloc((sizeof(char) * msgsize));
+	buf = (char *)malloc((sizeof(char) * msgsize));        /* Allocated buffer of specified size from user */
 
 	if ((s=socket(PF_INET, SOCK_DGRAM, 0))==-1)
+		perror("error in creating socket\n");
 
 	memset((char *) &server, 0, sizeof(server));
 	server.sin_family = AF_INET;
@@ -43,17 +44,17 @@ int main(int argc,char *argv[])
 		printf("bind error \n");
 
 	while(msgcount) {
-		recvd = recvfrom(s,buf, msgsize, 0,(struct sockaddr *)&si_other,&slen);
-		seqno = (buf[1] << 8 | buf[0]) ;
+		recvd = recvfrom(s,buf, msgsize, 0,(struct sockaddr *)&client,&slen);
+		seqno = (buf[1] << 8 | buf[0]);                                       /*LSB from first byte and MSB from second byte is used */
 		debug_print("sequence number:%d\r\n",seqno);
-		debug_print("recieved:%d bytes\n",recvd);
+		debug_print("received:%d bytes\n",recvd);
 		usleep(slptime * 1000);	
 		ack = seqno;	
-		sendto(s,&ack,sizeof(ack),0,(struct sockaddr *)&si_other,slen);
+		sendto(s,&ack,sizeof(ack),0,(struct sockaddr *)&client,slen);
 		msgcount--;
 	}
 	debug_print("Received packet from %s:%d\n\n\n", 
-			inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
+			inet_ntoa(client.sin_addr), ntohs(client.sin_port));
 	free(buf);	
 	close(s);
 	return 0;
